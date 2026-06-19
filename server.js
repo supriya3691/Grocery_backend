a5cc46af2fdb8932efdbdf09975a7a98d269fc08
Supriya <supriyareddy228@gmail.com>
2026-06-19 21:07:29 +0530

    port issue solved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
-const productRoutes = require("./Routes/productRoutes.js")
+const productRoutes = require("./routes/productRoutes.js")
 const path = require("path")
 const adminRoutes = require("./routes/adminRoutes.js")
 const emailRoutes = require("./routes/emailRoutes.js")
@@ -15,24 +15,24 @@ const app = express()
 console.log("checking", process.env.MONGO_URI)
 
 mongoose.connect(process.env.MONGO_URI)
-// console.log(MONGO_URI)
+    // console.log(MONGO_URI)
 
-.then(()=>{
-    console.log('database connected sucessfully')
-})
-.catch((error)=>{
-    console.log(error.message)
-})
+    .then(() => {
+        console.log('database connected sucessfully')
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
 
 app.use(express.json())
 app.use("/api", productRoutes)
-app.use("/admin", adminRoutes)    
-app.use("/email", emailRoutes)    
-app.use("/cart", cartRoutes)                                                                                                            
+app.use("/admin", adminRoutes)
+app.use("/email", emailRoutes)
+app.use("/cart", cartRoutes)
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
-const PORT = 3000;
-app.listen(PORT, ()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`server is ready: http://localhost:${PORT}`)
 })
